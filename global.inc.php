a50c8bec387deeafbd38ce3204d69e966c02312e
freshwired <edwardgao@yahoo.com>
2014-06-23 14:38:10 -0400

    Update global.inc.php

--- global.inc.php
+++ global.inc.php
@@ -13,8 +13,10 @@ $mysqli = new mysqli($host, $user, $password, $database);
 
 /**** check connection ****/
 if ($mysqli->connect_errno) {
+    
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
+    
 }
 
 ?>
